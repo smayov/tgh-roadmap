7644738574ace6ec385508956b6b4ea4bbbedd8b
smayov <santiagomayov@gmail.com>
2026-07-04 13:07:36 +0200

    Añadir import useRouter en catalogo

--- app/catalogo/page.jsx
+++ app/catalogo/page.jsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { useState, useEffect } from 'react';
+import { useRouter } from 'next/navigation';
 import { crearCheckout } from './checkout';
 import { supabase } from '../supabaseClient';
 

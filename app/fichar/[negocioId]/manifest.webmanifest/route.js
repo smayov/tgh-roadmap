6b37dde5646d202d650435a32ab106e48e78fb48
smayov <santiagomayov@gmail.com>
2026-07-26 15:33:38 +0200

    fichaje remoto; generación de banner5

--- app/fichar/[negocioId]/manifest.webmanifest/route.js
+++ app/fichar/[negocioId]/manifest.webmanifest/route.js
@@ -18,13 +18,13 @@ export async function GET(req, { params }) {
         src: "/icon-192-fichar.png",
         sizes: "192x192",
         type: "image/png",
-        purpose: "any maskable",
+        purpose: "any",
       },
       {
         src: "/icon-512-fichar.png",
         sizes: "512x512",
         type: "image/png",
-        purpose: "any maskable",
+        purpose: "any",
       },
     ],
   });

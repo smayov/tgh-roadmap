08f8970be8a7da0d93d3b9ecf001b2cadc7023f2
smayov <santiagomayov@gmail.com>
2026-08-17 11:59:45 +0200

    Auditoría de seguridad: migración a Supabase con RLS + modo cliente por código3

---
+++ app/roadmap/auditoria/page.jsx
@@ -0,0 +1,6 @@
+// app/roadmap/auditoria/page.jsx
+import AuditoriaSeguridadTGH from './auditoria-seguridad-tgh';
+
+export default function PaginaAuditoria() {
+  return <AuditoriaSeguridadTGH />;
+}
